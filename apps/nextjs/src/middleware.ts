73f97cc2dd58fad45bf0a1d172a1dddca816e4b5
Akbarshokh Sobirov <asobirov357@gmail.com>
2025-05-04 14:17:34 +0500

    feat: add simple middleware

---
+++ apps/nextjs/src/middleware.ts
@@ -0,0 +1,16 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export default function middleware(_req: NextRequest) {
+  return NextResponse.next();
+}
+
+// Or like this if you need to do something here.
+// export default auth((req) => {
+//   console.log(req.auth) //  { session: { user: { ... } } }
+// })
+
+// Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
